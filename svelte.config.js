8f444c71703274a15684d2d1d0d75ad444b12c37
Vladimir <vladimir.rdguez@gmail.com>
2022-12-18 09:39:29 -0500

    Initial svelte-kit

---
+++ svelte.config.js
@@ -0,0 +1,25 @@
+import adapter from '@sveltejs/adapter-static';
+const dev = "production" === "development";
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			pages: '',
+			assets: '',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: dev ? '' : '/ladetec',
+		}
+	}
+};
+
+export default config;
