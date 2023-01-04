6b4f2d8bebbbe6b39e862e18737b703b5432749e
Vladimir <vladimir.rdguez@gmail.com>
2023-01-03 21:11:34 -0500

    moving static to static dir

---
+++ _app/immutable/chunks/2-46317193.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-a85a5648.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		},
 		adapter: adapter({
 			pages: '',
-			assets: '',
+			assets: 'static',
 			fallback: null,
 			precompress: false,
 			strict: true
