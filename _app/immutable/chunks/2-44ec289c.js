2b0ea9a44ba21e7c648981cb54b2b8beb633bead
Vladimir <vladimir.rdguez@gmail.com>
2023-01-05 11:06:18 -0500

    Solves #2

---
+++ _app/immutable/chunks/2-44ec289c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-df269e24.js";export{t as component};
---
+++ _app/immutable/chunks/2-e19242e6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-e23f3e06.js";export{t as component};
